486b6cb80ff9f38649035354fd78588600db7de3
Jose Eladio Valenzuela <jvalenzuela@bal.com.mx>
2018-05-29 18:13:37 -0500

    Subir cambio de componente breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) {
+    this.router.events
+      .subscribe( event => {
+        console.log( event );
+      });
+  }
 
   ngOnInit() {
   }
